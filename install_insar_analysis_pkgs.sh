d4734a8990a96d857762260b57758efba929bd8f
Alex-Lewandowski <aflewandowski@alaska.edu>
2021-05-13 17:56:23 -0800

    add untested draft install_insar_analysis_pkgs.sh

---
+++ install_insar_analysis_pkgs.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+set -e
+env="insar_analysis"
+local="/home/jovyan/.local"
+env_prefix=$local"/envs/"$env
+site_packages=$env_prefix"/lib/python3.8/site-packages"
+
+######## ISCE ########
+
+# start building local path and pythonpath variables
+pythonpath=$site_packages/isce:$PYTHONPATH
+path=$site_packages/isce/applications:$PATH
+
+# set ISCE_HOME
+conda env config vars set -n $env ISCE_HOME=$site_packages/isce
+
+######## ARIA-Tools ########
+
+# clone the ARIA-Tools repo and build ARIA-Tools
+aria=$local"/ARIA-tools"
+if [ ! -d $aria ]
+then
+    git clone https://github.com/aria-tools/ARIA-tools.git $aria
+    conda run python $aria/setup.py build
+    conda run python $aria/setup.py install
+fi
+
+# clone the ARIA-tools-docs repo
+aria_docs="/home/jovyan/ARIA-tools-docs"
+if [ ! -d $aria_docs ]
+then
+    git clone -b master --depth=1 --single-branch https://github.com/aria-tools/ARIA-tools-docs.git $aria_docs
+fi
+
+######## MintPy ########
+
+mintpy=$local"/MintPy"
+pyaps=$local"/PyAPS"
+proj=$local"/envs/"$env"/share/proj"
+
+# set MintPy env variables
+conda env config vars set -n $env MINTPY_HOME=$mintpy
+conda env config vars set -n $env PYAPS_HOME=$pyaps
+conda env config vars set -n $env PROJ_LIB=$proj
+
+#update local path and pythonpath variables
+path=$mintpy"/mintpy:"$path
+pythonpath=$mintpy":"$pyaps":"$pythonpath
+
+# clone MintPy
+if [ ! -d $mintpy ]
+then
+    git clone -b v1.3.0 --depth=1 --single-branch https://github.com/insarlab/MintPy.git $mintpy
+fi
+
+# clone pyaps
+if [ ! -d $pyaps ]
+then
+    git clone -b main --depth=1 --single-branch https://github.com/yunjunz/pyaps3.git $pyaps
+fi
+
+# install pykml
+$env_prefix/bin/pip install pykml -e git+https://github.com/yunjunz/pykml.git#egg=pykml
+
+######## TRAIN ########
+
+train=$local"/TRAIN"
+
+#update local pythonpath variable
+pythonpath=$train"/src:"$pythonpath
+
+# clone TRAIN patched for Python3 in the OpenSARlab
+if [ ! -d $train ]
+then
+    git clone -b OpenSARlab --single-branch https://github.com/asfadmin/hyp3-TRAIN.git $train
+fi
+
+#######################
+
+# set PATH and PYTHONPATH
+conda env config vars set -n $env PYTHONPATH=$pythonpath
+conda env config vars set -n $env PATH=$path
