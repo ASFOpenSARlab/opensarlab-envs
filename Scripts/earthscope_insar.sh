df3ba1b09c6675abe6f03d5bcdbbabeb02540bd1
Alex-Lewandowski <aflewandowski@alaska.edu>
2024-08-02 22:46:44 +0000

    install aria-tools and autorift with conda

--- Scripts/earthscope_insar.sh
+++ Scripts/earthscope_insar.sh
@@ -11,71 +11,27 @@ LOCAL="$HOME"/.local
 NAME="earthscope_insar"
 python_version=$(conda run -n $NAME python --version | cut -b 8-11)
 SITE_PACKAGES="$LOCAL/envs/$NAME/lib/python"$python_version"/site-packages"
-##############################################################
-
-#conda run -n "$NAME" kernda --display-name $NAME $LOCAL/envs/$NAME/share/jupyter/kernels/python3/kernel.json -o
 
 pythonpath="$PYTHONPATH"
 path="$PATH"
 
 ######## Set ISCE env vars ########
 
-# start building local path and pythonpath variables
+# set path and pythonpath variables
 pythonpath=$SITE_PACKAGES/isce:"$pythonpath"
 path="$SITE_PACKAGES"/isce/applications:"$LOCAL"/envs/"$NAME"/bin:$path
+conda env config vars set -n $NAME PYTHONPATH="$pythonpath"
+conda env config vars set -n $NAME PATH="$path"
 
 # set ISCE_HOME
 conda env config vars set -n $NAME ISCE_HOME="$SITE_PACKAGES"/isce
 
-######## Install ARIA-Tools ########
-
-# clone the ARIA-Tools repo and build ARIA-Tools
-aria="$LOCAL/ARIA-tools"
-if [ ! -d "$aria" ]
-then
-    git clone -b v1.2 https://github.com/aria-tools/ARIA-tools.git "$aria"
-    wd=$(pwd)
-    cd "$aria"
-    conda run -n $NAME python "$aria"/setup.py build
-    conda run -n $NAME python "$aria"/setup.py install
-    cd "$wd"
-fi
-
-path="$LOCAL/ARIA-tools/tools/bin:$LOCAL/ARIA-tools/tools/ARIAtools:"$path
-pythonpath="$LOCAL/ARIA-tools/tools:$LOCAL/ARIA-tools/tools/ARIAtools:"$pythonpath
-conda env config vars set -n $NAME GDAL_HTTP_COOKIEFILE=/tmp/cookies.txt
-conda env config vars set -n $NAME GDAL_HTTP_COOKIEJAR=/tmp/cookies.txt
-conda env config vars set -n $NAME VSI_CACHE=YES
-
-# clone the ARIA-tools-docs repo
+####### Clone the ARIA-tools-docs repo ########
 aria_docs="/home/jovyan/ARIA-tools-docs"
 if [ ! -d $aria_docs ]
 then
     git clone -b master --depth=1 --single-branch https://github.com/aria-tools/ARIA-tools-docs.git $aria_docs
 fi
-
- ######## Install autoRIFT ########
-
- # clone the autorift-Tools repo and build autorift-Tools
- autorift="$LOCAL/autoRIFT"
- cc="$LOCAL"/envs/"$NAME"/bin/x86_64-conda_cos6-linux-gnu-gcc
- cxx="$LOCAL"/envs/"$NAME"/bin/x86_64-conda_cos6-linux-gnu-g++
- include="$LOCAL"/envs/"$NAME"/include/opencv4
- if [ ! -d "$autorift" ]
- then
-     git clone https://github.com/nasa-jpl/autoRIFT.git "$autorift"
-     wd=$(pwd)
-     cd "$autorift"
-     conda run -n $NAME CC="$cc" CXX="$cxx" CFLAGS=-I${include} python "$autorift"/setup.py build
-     conda run -n $NAME python -m pip install .
-     cd "$wd"
- fi
-
-#######################
-
-# set PATH and PYTHONPATH
-conda env config vars set -n $NAME PYTHONPATH="$pythonpath"
-conda env config vars set -n $NAME PATH="$path"
 
 
 BASH_PROFILE=$HOME/.bash_profile
